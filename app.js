48903223302172c004076380d2fbcab3429ef291
Jan-Hendrik Grundhöfer <jan-hendrik.grundhoefer@web.de>
2018-12-12 19:36:08 +0100

    Introduce an http endpoint.

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const fs = require('fs'),
 const flaschenpost = require('flaschenpost'),
       getCorsOrigin = require('get-cors-origin'),
       processenv = require('processenv'),
-      spdy = require('spdy'),
       tailwind = require('tailwind'),
       Value = require('validate-value');
 
@@ -24,12 +23,9 @@ const readFile = promisify(fs.readFile);
 
 const logger = flaschenpost.getLogger();
 
-const keysDirectory = processenv('KEYS');
-
 const addFileAuthorizationOptions = processenv('IS_AUTHORIZED_COMMANDS_ADD_FILE', { forAuthenticated: true, forPublic: false }),
       apiCorsOrigin = getCorsOrigin(processenv('API_CORS_ORIGIN')),
       httpPort = processenv('HTTP_PORT', 80),
-      httpsPort = processenv('HTTPS_PORT', 443),
       identityProviderCertificatePath = processenv('IDENTITYPROVIDER_CERTIFICATE'),
       identityProviderName = processenv('IDENTITYPROVIDER_NAME'),
       statusCorsOrigin = processenv('STATUS_CORS_ORIGIN', '*'),
@@ -39,9 +35,6 @@ const providerConfiguration = getProviderConfiguration();
 
 (async () => {
   try {
-    const certificate = await readFile(path.join(keysDirectory, 'certificate.pem'), { encoding: 'utf8' }),
-          privateKey = await readFile(path.join(keysDirectory, 'privateKey.pem'), { encoding: 'utf8' });
-
     const identityProviderCertificate = await readFile(path.join(identityProviderCertificatePath, 'certificate.pem'));
 
     const value = new Value({
@@ -80,17 +73,6 @@ const providerConfiguration = getProviderConfiguration();
       }
     });
 
-    await new Promise((resolve, reject) => {
-      try {
-        spdy.createServer({
-          key: privateKey,
-          cert: certificate
-        }, api).listen(httpsPort, resolve);
-      } catch (ex) {
-        reject(ex);
-      }
-    });
-
     const app = tailwind.createApp({});
 
     await app.status.use(new app.wires.status.http.Server({
@@ -98,12 +80,7 @@ const providerConfiguration = getProviderConfiguration();
       corsOrigin: statusCorsOrigin
     }));
 
-    logger.info('Server started.', {
-      ports: {
-        https: httpsPort,
-        http: httpPort
-      }
-    });
+    logger.info('Server started.', { httpPort });
   } catch (ex) {
     logger.fatal('An unexpected error occured.', { err: ex });
 
